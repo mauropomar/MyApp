a2ccaf01dbfeccd85b3ce497d22c45925b50205b
mauro.pomar <mauro.pomar@datazucar.cu>
2021-05-20 16:53:14 -0500

    Primeros Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {PerfilComponent} from './components/perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [{
+      path: 'perfil',
+      component: PerfilComponent,
+      data: {title: 'Perfil'}
+    }]
+  },
+  { path: '',   redirectTo: '/home/perfil', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button'
+
+
+import { PerfilComponent } from './components/perfil/perfil.component';
+
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PerfilComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
+  ],
+  exports:[
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
